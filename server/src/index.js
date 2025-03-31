38f963e646a128e51b9290ca612a96400be2596b
PhillipDaum <philgdaum@gmail.com>
2025-03-31 13:56:23 -0600

    add version 5

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ const { Client } = pg; //external module for using postgres with node
 
 const config = {
   user: process.env.USER,
-  host: process.env.host,
+  host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: process.env.DATABASE_PORT,
